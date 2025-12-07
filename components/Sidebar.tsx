b2673d0b65e08bd4c953f9de3b7240bde4b16806
hummingbirdtestai <hummingbirdtestai@gmail.com>
2025-12-07 11:56:46 +0530

    Updated Sidebar.tsx

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -23,14 +23,6 @@ interface NavItem {
   icon: any;
 }
 
-/* const navItems: NavItem[] = [
-  { label: 'Home', href: '/', icon: Home },
-  { label: 'Concepts', href: '/practice', icon: BookOpen },
-  { label: 'Flash Cards', href: '/flashcard-feed-demo', icon: CreditCard },
-  { label: 'NEET-PG Full-Scale Mock Tests', href: '/mocktests', icon: FileText },
-  { label: 'Battles', href: '/battles', icon: Swords },
-  { label: 'Analytics', href: '/analytics', icon: BarChart3 },
-]; */
 const navItems: NavItem[] = [
   { id: "home", label: "Home", href: "/", icon: Home },
   { id: "practice", label: "Concepts", href: "/practice", icon: BookOpen },
