e001b4b4ea33fad6c152dac9d2ba90e43625d16c
hummingbirdtestai <hummingbirdtestai@gmail.com>
2025-12-13 12:39:41 +0530

    Updated UnifiedVideoPlayer.tsx

---
+++ components/video/UnifiedVideoPlayer.tsx
@@ -0,0 +1,30 @@
+import { Platform } from "react-native";
+import BunnyWebPlayer from "./BunnyWebPlayer";
+import BunnyMobilePlayer from "./BunnyMobilePlayer";
+
+export default function UnifiedVideoPlayer({
+  videoUrl,
+  posterUrl,
+  onProgress,
+  onEnded,
+}) {
+  if (Platform.OS === "web") {
+    return (
+      <BunnyWebPlayer
+        videoUrl={videoUrl}
+        posterUrl={posterUrl}
+        onProgress={onProgress}
+        onEnded={onEnded}
+      />
+    );
+  }
+
+  return (
+    <BunnyMobilePlayer
+      videoUrl={videoUrl}
+      posterUrl={posterUrl}
+      onProgress={onProgress}
+      onEnded={onEnded}
+    />
+  );
+}
