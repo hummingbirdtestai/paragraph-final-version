51f9f8ca44ee82406fe40d924fbb034f517a4360
hummingbirdtestai <hummingbirdtestai@gmail.com>
2025-12-18 13:27:45 +0530

    Updated useImageData.ts

--- hooks/useImageData.ts
+++ hooks/useImageData.ts
@@ -2,7 +2,7 @@
 import { useEffect, useState } from "react";
 import { supabase } from "@/lib/supabaseClient";
 
-export function useVideoData(
+export function useImageData(
   subject = null,
   userId = null,
   category = "unviewed"
