8ffecb5ff580c6b3f061f4977ea09aed8f701f06
hummingbirdtestai <hummingbirdtestai@gmail.com>
2025-12-20 00:46:29 +0530

    Updated settings.tsx

--- app/settings.tsx
+++ app/settings.tsx
@@ -286,7 +286,7 @@ const confirmLogout = async () => {
   return (
     <MainLayout>
     <SafeAreaView style={styles.container}>
-      <PageHeader title="Settings" />
+      <PageHeader title="Settings" showLogo={false} />
 
       <ScrollView
         style={styles.scrollView}
