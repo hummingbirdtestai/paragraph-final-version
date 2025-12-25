968d91e312163c87a5308711abba411093567960
hummingbirdtestai <hummingbirdtestai@gmail.com>
2025-12-25 14:03:54 +0530

    Updated index.tsx

--- app/index.tsx
+++ app/index.tsx
@@ -5,13 +5,13 @@ import MainLayout from '@/components/MainLayout';
 
 export default function Index() {
   const images = {
-    img1: 'https://qyhbwuqnedkizvvsyfyx.supabase.co/storage/v1/object/public/medical-images/7.webp?width=900&quality=70&format=webp',
-    img2: 'https://qyhbwuqnedkizvvsyfyx.supabase.co/storage/v1/object/public/medical-images/2.webp?width=900&quality=70&format=webp',
-    img3: 'https://qyhbwuqnedkizvvsyfyx.supabase.co/storage/v1/object/public/medical-images/3.webp?width=900&quality=70&format=webp',
-    img4: 'https://qyhbwuqnedkizvvsyfyx.supabase.co/storage/v1/object/public/medical-images/4.webp?width=900&quality=70&format=webp',
-    img5: 'https://qyhbwuqnedkizvvsyfyx.supabase.co/storage/v1/object/public/medical-images/5.webp?width=900&quality=70&format=webp',
-    img6: 'https://qyhbwuqnedkizvvsyfyx.supabase.co/storage/v1/object/public/medical-images/6.webp?width=900&quality=70&format=webp',
-  };
+  img1: 'https://qyhbwuqnedkizvvsyfyx.supabase.co/storage/v1/object/public/Home%20page%20images/1.webp',
+  img2: 'https://qyhbwuqnedkizvvsyfyx.supabase.co/storage/v1/object/public/Home%20page%20images/2.webp',
+  img3: 'https://qyhbwuqnedkizvvsyfyx.supabase.co/storage/v1/object/public/Home%20page%20images/3.webp',
+  img4: 'https://qyhbwuqnedkizvvsyfyx.supabase.co/storage/v1/object/public/Home%20page%20images/4.webp',
+  img5: 'https://qyhbwuqnedkizvvsyfyx.supabase.co/storage/v1/object/public/Home%20page%20images/5.webp',
+  img6: 'https://qyhbwuqnedkizvvsyfyx.supabase.co/storage/v1/object/public/Home%20page%20images/6.webp',
+};
 
   return (
     <MainLayout>
